adcff69e0fed33c4c5dd65f4c8473a9f402ab883
Daniel J <danieldrdj01@gmail.com>
2021-08-12 00:47:25 -0700

    Completed a page for my work experience at the BWKI
    Added pictures for the KI and created a slideshow with all of the pictures. I created the html to display and gave quick descriptions of my jobs there.

---
+++ javaScript/bestWesternKodiakInn.js
@@ -0,0 +1,22 @@
+//This file has any and all JavaScript required for the Best Western Kodiak Inn page
+
+//Section 1: Simple Image Slider
+
+var slideIndex = 1;
+
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length};
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
